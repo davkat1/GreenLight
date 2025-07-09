1bab01dcb85188c91f61d76020028ec35ffba64a
Shanaka Prageeth <Shanakaprageeth@gmail.com>
2025-07-09 10:43:01 +0000

    add energy data

--- scripts/epw_to_csv.py
+++ scripts/epw_to_csv.py
@@ -56,6 +56,7 @@ def epw_to_csv(epw_path, csv_path):
 
 if __name__ == "__main__":
     print("DO NOT Use this script use energy plus tool. TODO fix the format")
+    sys.exit(0)
     import argparse
 
     parser = argparse.ArgumentParser(description="Convert EPW weather file to CSV.")
